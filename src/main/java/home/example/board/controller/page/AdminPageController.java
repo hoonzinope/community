b8ecede5fb5d868bdc20bf9a9e055301d4592ee2
hoonzinope <hoonzinope@gmail.com>
2025-05-11 10:05:57 +0900

    Implement admin role validation in AdminPageController

--- src/main/java/home/example/board/controller/page/AdminPageController.java
+++ src/main/java/home/example/board/controller/page/AdminPageController.java
@@ -1,5 +1,7 @@
 package home.example.board.controller.page;
 
+import home.example.board.DTO.CustomUserDetail;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,32 +9,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminPageController {
 
     @GetMapping("/admin")
-    public String adminPage() {
+    public String adminPage(@AuthenticationPrincipal CustomUserDetail userDetail) {
+        isAdmin(userDetail);
         return "redirect:/admin/users";
     }
 
     @GetMapping("/admin/users")
-    public String adminUsersPage() {
+    public String adminUsersPage(@AuthenticationPrincipal CustomUserDetail userDetail) {
+        isAdmin(userDetail);
         return "admin/user/admin_user";
     }
 
     @GetMapping("/admin/posts")
-    public String adminPostsPage() {
+    public String adminPostsPage(@AuthenticationPrincipal CustomUserDetail userDetail) {
+        isAdmin(userDetail);
         return "admin/post/admin_post";
     }
 
     @GetMapping("/admin/comments")
-    public String adminCommentsPage() {
+    public String adminCommentsPage(@AuthenticationPrincipal CustomUserDetail userDetail) {
+        isAdmin(userDetail);
         return "admin/comment/admin_comment";
     }
 
     @GetMapping("/admin/subjects")
-    public String adminSubjectsPage() {
+    public String adminSubjectsPage(@AuthenticationPrincipal CustomUserDetail userDetail) {
+        isAdmin(userDetail);
         return "admin/admin_subject";
     }
 
     @GetMapping("/admin/statistics")
-    public String adminStatisticsPage() {
+    public String adminStatisticsPage(@AuthenticationPrincipal CustomUserDetail userDetail) {
+        isAdmin(userDetail);
         return "admin/admin_stat";
+    }
+
+    private void isAdmin(CustomUserDetail userDetail) {
+        if (userDetail == null) {
+            throw new IllegalArgumentException("User detail cannot be null");
+        }
+        if (!userDetail.isAdmin()) {
+            throw new IllegalArgumentException("User is not an admin");
+        }
     }
 }
